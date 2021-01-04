e9c497efd8dfe7a41f003aede0fe954faf294131
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2021-01-04 21:25:52 +0900

    The End of ch18

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 
@@ -7,3 +8,8 @@ class User(models.Model):
     USERNAME_FIELD = "email"
     is_anonymous = False
     is_authenticated = True
+
+
+class Token(models.Model):
+    email = models.EmailField(default="")
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
