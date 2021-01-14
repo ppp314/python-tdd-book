5b205433a691aa73d3187aa1301adf1f32226d1e
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2021-01-15 06:18:52 +0900

    Adapting Our FT to Be Able to Test Real Emails via POP3

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,28 +7,29 @@ import time
 MAX_WAIT = 10
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get("STAGING_SERVER")
-        if staging_server:
-            self.live_server_url = "http://" + staging_server
+        self.staging_server = os.environ.get("STAGING_SERVER")
+        if self.staging_server:
+            self.live_server_url = "http://" + self.staging_server
 
     def tearDown(self):
         self.browser.quit()
-
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-
-        return modified_fn
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
