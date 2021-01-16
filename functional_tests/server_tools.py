d2c1d44c586f02c11e884c1b07aa2c15921ca376
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2021-01-17 06:15:16 +0900

    Use json.load instead of dict

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,3 +1,4 @@
+import json
 from fabric.api import run
 from fabric.context_managers import settings, shell_env
 
@@ -14,7 +15,7 @@ def reset_database(host):
 
 def _get_server_env_vars(host):
     env_lines = run(f"cat ~/sites/{host}/.env").splitlines()
-    return dict(l.split("=") for l in env_lines if l)
+    return json.loads(l.split("=") for l in env_lines if l)
 
 
 def create_session_on_server(host, email):
