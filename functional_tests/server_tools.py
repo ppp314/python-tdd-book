fadbe6dbe714229bf37d0a2aba7ce778365a392d
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2021-01-17 06:39:28 +0900

    Use pprint

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,3 +1,4 @@
+import pprint
 from fabric.api import run
 from fabric.context_managers import settings, shell_env
 
@@ -14,7 +15,7 @@ def reset_database(host):
 
 def _get_server_env_vars(host):
     env_lines = run(f"cat ~/sites/{host}/.env").splitlines()
-    print (l.split("=") for l in env_lines if l)
+    pprint.pp(l.split("=") for l in env_lines if l)
     return dict(l.split("=") for l in env_lines if l)
 
 
