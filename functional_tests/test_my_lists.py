9415b0a58d4f2341131de9943c65ac40b7a261d0
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2021-01-18 20:50:23 +0900

    Remove these lines since they are irrelevant

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,10 +1,7 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from .base import FunctionalTest
 from .server_tools import create_session_on_server
 from .management.commands.create_session import create_pre_authenticated_session
-
-User = get_user_model()
 
 
 class MyListsTest(FunctionalTest):
