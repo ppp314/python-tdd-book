447c9d740f1d0a56bf531d258cb8c6dca939cf4d
37033555+ppp314 <37033555+ppp314@users.noreply.github.com>
2021-01-28 12:47:27 +0900

    lists can have owners, which are saved on creation.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -162,8 +162,6 @@ class MyListsTest(TestCase):
     def test_list_owner_is_saved_if_user_is_authenticated(self):
         user = User.objects.create(email="a@b.com")
         self.client.force_login(user)
-        self.client.post("/list/new", data={"text": "new item"})
+        self.client.post("/lists/new", data={"text": "new item"})
         list_ = List.objects.first()
         self.assertEqual(list_.owner, user)
-
-
